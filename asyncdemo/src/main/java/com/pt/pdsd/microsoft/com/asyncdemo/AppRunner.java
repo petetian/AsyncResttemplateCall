8377d185e970cd43a48833ebb69831f0eebd5324
Pete T <pete.tian@gmail.com>
2020-05-21 12:39:08 -0500

    Add source code

---
+++ asyncdemo/src/main/java/com/pt/pdsd/microsoft/com/asyncdemo/AppRunner.java
@@ -0,0 +1,38 @@
+package com.pt.pdsd.microsoft.com.asyncdemo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.util.concurrent.CompletableFuture;
+
+@Component
+public class AppRunner implements CommandLineRunner {
+
+    private static final Logger logger = LoggerFactory.getLogger(AppRunner.class);
+
+    private final GitHubLookupService gitHubLookupService;
+
+    public AppRunner(GitHubLookupService gitHubLookupService) {
+        this.gitHubLookupService = gitHubLookupService;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        long start = System.currentTimeMillis();
+
+        CompletableFuture<User> page1 = gitHubLookupService.findUser("PivotalSoftware");
+        CompletableFuture<User> page2 = gitHubLookupService.findUser("Microsoft");
+        CompletableFuture<User> page3 = gitHubLookupService.findUser("petetian");
+
+        CompletableFuture.allOf(page1, page2, page3).join();
+
+        logger.info("Elapsed time: " +
+                (System.currentTimeMillis() - start));
+        logger.info("--> " + page1.get());
+        logger.info("--> " + page2.get());
+        logger.info("--> " + page3.get());
+    }
+}
---
+++ asyncdemo/src/main/java/com/pt/pdsd/microsoft/com/asyncdemo/AsyncdemoApplication.java
@@ -0,0 +1,27 @@
+package com.pt.pdsd.microsoft.com.asyncdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@SpringBootApplication
+@EnableAsync
+public class AsyncdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsyncdemoApplication.class, args).close();
+	}
+
+	@Bean
+	public ThreadPoolTaskExecutor taskExecutor() {
+		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+		executor.setCorePoolSize(2);
+		executor.setMaxPoolSize(2);
+		executor.setQueueCapacity(500);
+		executor.setThreadNamePrefix("GithubLookup-");
+		executor.initialize();
+		return executor;
+	}
+}
