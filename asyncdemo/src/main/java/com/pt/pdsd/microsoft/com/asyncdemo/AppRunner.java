a5338c2f6979e511e73676ebf4cdb0e5e10ae5f2
Pete T <pete.tian@gmail.com>
2021-02-09 20:14:45 -0600

    Refactor

--- asyncdemo/src/main/java/com/pt/pdsd/microsoft/com/asyncdemo/AppRunner.java
+++ asyncdemo/src/main/java/com/pt/pdsd/microsoft/com/asyncdemo/AppRunner.java
@@ -1,12 +1,11 @@
 package com.pt.pdsd.microsoft.com.asyncdemo;
 
+import java.util.concurrent.CompletableFuture;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
-
-import java.time.LocalDate;
-import java.util.concurrent.CompletableFuture;
 
 @Component
 public class AppRunner implements CommandLineRunner {
@@ -29,10 +28,10 @@ public class AppRunner implements CommandLineRunner {
 
         CompletableFuture.allOf(page1, page2, page3).join();
 
-        logger.info("Elapsed time: " +
+        logger.info("Elapsed time: {}",
                 (System.currentTimeMillis() - start));
-        logger.info("--> " + page1.get());
-        logger.info("--> " + page2.get());
-        logger.info("--> " + page3.get());
+        logger.info("--> {}", page1.get());
+        logger.info("--> {}", page2.get());
+        logger.info("--> {}", page3.get());
     }
 }
